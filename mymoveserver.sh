f13bd02fa52bffde6e0222d543c1e15e7e0eb0c9
Topi Santakivi <topi.santakivi@gmail.com>
2011-10-22 22:48:18 +0300

    Tune 3-finger network, add .conf file updating logic. Ready for 0.1.3

--- mymoveserver.sh
+++ mymoveserver.sh
@@ -4,6 +4,40 @@ echo "Moving mymoves.conf to right place if it exists..."
 [[ -e /home/user/MyDocs/.moves/mymoves.conf ]] && cat /home/user/MyDocs/.moves/mymoves.conf > /home/user/.config/mymoves.conf
 [[ -d /home/user/MyDocs/.moves ]] && rm -rf /home/user/MyDocs/.moves
 
+mmconf=/home/user/.config/mymoves.conf
+
+if [ -f $mmconf ]
+then
+    if [ `head -n1 $mmconf` = "0" ]
+    then
+        echo "Updating mymoves.conf to the newest version..."
+        # Update mymoves.conf to the version 1 format
+        echo 1 > $mmconf.new
+        head -5 $mmconf | tail -4 >> $mmconf.new
+        echo "d4######" >> $mmconf.new
+        echo "d5######" >> $mmconf.new
+        echo "d6######" >> $mmconf.new
+        head -7 $mmconf | tail -1 | sed -e 's/d5###/d7###/' >> $mmconf.new
+        echo "d8######" >> $mmconf.new
+        echo "d9######" >> $mmconf.new
+        echo "d10######" >> $mmconf.new
+        head -6 $mmconf | tail -1 | sed -e 's/d4###/d11###/' >> $mmconf.new
+
+        head -11 $mmconf | tail -4 >> $mmconf.new
+        echo "t4######" >> $mmconf.new
+        echo "t5######" >> $mmconf.new
+        echo "t6######" >> $mmconf.new
+        head -13 $mmconf | tail -1 | sed -e 's/t5###/t7###/'>> $mmconf.new
+        echo "t8######" >> $mmconf.new
+        echo "t9######" >> $mmconf.new
+        echo "t10######" >> $mmconf.new
+        head -12 $mmconf | tail -1 | sed -e 's/t4###/t11###/'>> $mmconf.new
+
+        rm $mmconf
+        mv $mmconf.new $mmconf
+    fi
+fi
+
 echo "Starting mymoveserver"
 
 exec /opt/mymoves/bin/mymoveserver
--- nn_training/multinn/08/3finger/train.c
+++ nn_training/multinn/08/3finger/train.c
@@ -22,7 +22,7 @@ int main()
 //					  train_data->num_input, num_neurons_hidden, train_data->num_output);
 
     //ann = fann_create_standard(500, 2, 50, 50, 21);
-    unsigned int layers[4] = {600, 200, 64, 12};
+    unsigned int layers[4] = {600, 100, 24, 12};
     ann = fann_create_standard_array(4, layers);
 	printf("Training network.\n");
 
