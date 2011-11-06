6f5ebaf22ada4ea75f47990405d5219ebd6914f2
Topi Santakivi <topi.santakivi@gmail.com>
2011-11-06 11:59:59 +0200

    Update the neural networks with better training data

---
+++ nn_training/multinn/feat02/2finger/collect_training_data.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+amount=12
+samples=15
+
+for x in `seq 0 $(($amount-1))`
+#for x in `seq 9 11`
+do
+    /opt/mymoves/bin/mymoveserver $x $amount $samples
+    cat gesturedata$x >> ann_training_data
+done
+
